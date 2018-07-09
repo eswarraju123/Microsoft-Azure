4e1f6d7149bc67d10481e6abf245e1f61a55b8dc
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2018-07-09 11:11:37 +0530

    Form Flow using Cortana : Azure Bot Framework
    C# sample to register a user with voice-based commands (input) using Azure Bot Framework (Cortana Channel)

---
+++ Members.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Azure;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Builder.FormFlow;
+using System;
+using System.Threading.Tasks;
+
+namespace AdaptiveSpeak.Models
+{
+    [Serializable]
+    public class Members
+    {
+        [Prompt("May I know your good name?")]
+        public string Name { get; set; }
+
+        [Prompt("What is your contact number?")]
+        [Numeric(6000000000, 9999999999)]
+        public double PhoneNumber { get; set; }
+
+        [Prompt("What is your address?")]
+        public string Address { get; set; }
+
+        public static IForm<Members> BuildForm() => new FormBuilder<Members>()
+            .Message("Wipro voice based Registration")
+            .Field(nameof(Name))
+            .Field(nameof(PhoneNumber))
+            .Field(nameof(Address))
+            .Confirm(async (state) =>
+            {
+                return new PromptAttribute("Hi {Name}, Please review your selection. Phone Number: {PhoneNumber}, Address: {Address}. Do you want to continue? {||}");
+            })
+            .OnCompletion(saveDetails)
+            .Build();
+
+        private static Task saveDetails(IDialogContext context, Members state)
+        {
+            context.SayAsync(text: state.Name, speak: state.Name);
+            return Task.CompletedTask;
+        }
+    }
+}
