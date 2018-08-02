f2bbcafcaedaea343be85a820d1c2e23b3bd9f66
Sairam Sekhar Poluru <poluru.sairam@gmail.com>
2018-08-02 18:36:11 +0530

    Azure Storage Blob : Container Operations using C#

---
+++ Storage/Blob/Program.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StorageBlobContainer
+{
+    class Program
+    {
+        static CloudStorageAccount storageAccount;
+        static CloudBlobClient blobClient;
+        static CloudBlobContainer blobContainer;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=========================================");
+            Console.WriteLine("Azure Storage Blob Container Operations");
+            Console.WriteLine("=========================================");
+
+            while (true)
+            {
+                string containerName = null;
+                storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=networkdiag484;AccountKey=2SMVYxUilSV45OjURssEhufpf85WamWOP5igyMNMOnjPw6QZZwZQI1ArSraXLCsZcqVTqH93jg4GG39ftw2jMw==;EndpointSuffix=core.windows.net");
+                blobClient = storageAccount.CreateCloudBlobClient();
+                Console.WriteLine("\n1. Create a container\n2. View Containers\n3. Delete a container\n4. Exit\n");
+                Console.Write("Enter your Option : ");
+                switch (Convert.ToInt32(Console.ReadLine()))
+                {
+                    case 1:
+                        // Create a container if it doesn't exist.
+                        Console.Write("Enter name of Container : ");
+                        containerName = Console.ReadLine();
+                        blobContainer = blobClient.GetContainerReference(containerName);
+                        blobContainer.CreateIfNotExistsAsync();
+                        blobContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Blob });
+                        Console.WriteLine("Container created successfully.");
+                        break;
+                    case 2:
+                        // View all containers
+                        IEnumerable<CloudBlobContainer> containers = blobClient.ListContainers();
+                        foreach (CloudBlobContainer list in containers)
+                        {
+                            Console.WriteLine("Container Name : " + list.Name + "\tContainer Link: " + list.Uri);
+                        }
+                        break;
+                    case 3:
+                        // Delete a container
+                        Console.Write("Enter name of Container : ");
+                        containerName = Console.ReadLine();
+                        blobContainer = blobClient.GetContainerReference(containerName);
+                        blobContainer.DeleteIfExistsAsync();
+                        Console.WriteLine("Container deleted successfully.");
+                        break;
+                    case 4:
+                        System.Environment.Exit(1);
+                        break;
+                }
+            }
+        }
+    }
+}
